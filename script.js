1dec9d2017c1bf9fe3c63a45b7956318fd68761c
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2023-04-12 08:29:39 +0100

    get lat and lon from user input working

--- script.js
+++ script.js
@@ -7,12 +7,39 @@ let APIKey = "1f905548b7101976aa855eb9b92ca7ad";
 let cities = [];
 let locations = [];
 let queryUrl = "";
+let latitude; 
+let longitude;
+
+let dateNow = moment().format('DD/MM/YYYY, h:mm a');
+// response.list["dt_txt"];
+
 searchButton.click(function(event){
     event.preventDefault();
     let userInput = searchInput.val();
     cities.push(userInput);
+    // url for today weather api
     let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + APIKey + "&units=metric"
     console.log(queryUrl);
+
+    //to get lat and lon from user input town
+    let latUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&appid=" + APIKey;
+
+    function getForecast(){
+        $.ajax({
+            url: latUrl,
+            method: "GET"
+        }).then(function(response){
+            let latitude = response[0].lat;
+            let longitude = response[0].lon;
+            // console.log(latitude);
+            // console.log(longitude);
+            let forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey;
+            console.log(forecastUrl);
+            return forecastUrl;
+        })
+    
+    }
+
 
     function getToday(){
         $.ajax({
@@ -23,7 +50,7 @@ searchButton.click(function(event){
             $("#temp-today").html(`Temp: ${Math.floor(response.main.temp)}`);
             $("#wind-today").html(`Wind: ${Math.floor(response.wind.speed)}`);
             $("#humidity-today").html(`Humidity: ${Math.floor(response.main.humidity)}`);
-            $("#current-town").html(userInput);
+            $("#current-town").html(`${userInput} ${dateNow}`);
             // console.log(response.main.temp);
             // console.log(response.wind.speed);
             // console.log(response.main.humidity)
